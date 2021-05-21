5871fda459df25fd55e3e086a68afaab0ee5da92
lucaxue <lxue02@gmail.com>
2021-05-21 17:35:19 +0100

    removed unnecessary imports

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
-use App\Models\Book;
 use App\Repository\BookRepositoryInterface;
 
 class BookController extends Controller

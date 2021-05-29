5ee473700ef23ce88661944d2e009ea94a1df051
Luca Xue <lxue02@gmail.com>
2021-05-29 17:14:13 +0100

    test refactor

--- tests/Unit/BookControllerTest.php
+++ tests/Unit/BookControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Unit;
 
 use App\Repository\BookRepositoryInterface;
-use Illuminate\Http\JsonResponse;
 use Mockery;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -11,20 +10,20 @@ use Tests\TestCase;
 class BookControllerTest extends TestCase
 {
 
-    private $_books = [
+    private $books = [
         [
             "id" => 1,
             "title" => "Nice Book Title",
             "author" => "Dr. Jedidiah Haag Jr.",
             "created_at" => "2021-05-21T12:16:51.000000Z",
-            "updated_at" => "2021-05-21T12:16:51.000000Z"
+            "updated_at" => "2021-05-21T12:16:51.000000Z",
         ],
         [
             "id" => 2,
             "title" => "Nice Second Book Title",
             "author" => "Dr. Jedidiah Haag Jr.",
             "created_at" => "2021-05-21T12:16:51.000000Z",
-            "updated_at" => "2021-05-21T12:16:51.000000Z"
+            "updated_at" => "2021-05-21T12:16:51.000000Z",
         ],
     ];
     /**
@@ -38,16 +37,15 @@ class BookControllerTest extends TestCase
             BookRepositoryInterface::class,
             Mockery::mock(
                 BookRepositoryInterface::class,
-                function (MockInterface $mock) {
-                    $mock->shouldReceive('all')
-                        ->andReturn($this->_books);
-                }
+                fn(MockInterface $mock) => $mock
+                    ->shouldReceive('all')
+                    ->andReturn($this->books)
             )
         );
 
         $response = $this->get('api/books');
 
-        $response->assertJson($this->_books);
+        $response->assertJson($this->books);
         $response->assertStatus(200);
     }
 }

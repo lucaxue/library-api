557916e919fa5b05e22a29dd6ffd25f6128522af
Luca Xue <lxue02@gmail.com>
2021-08-30 13:25:09 +0100

    Use books api resource route

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::apiResource('/books', BookController::class);

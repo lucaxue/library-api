f3efb2c478425efbe5079282e650f003a2e41102
Luca Xue <lxue02@gmail.com>
2021-08-30 13:26:34 +0100

    Reformat

--- app/Repository/EloquentRepositoryInterface.php
+++ app/Repository/EloquentRepositoryInterface.php
@@ -7,51 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 
 interface EloquentRepositoryInterface
 {
-  /**
-   * Gets all models.
-   * 
-   * @return Illuminate\Database\Eloquent\Collection|array;
-   */
-  public function all(): Collection|array;
+    /**
+     * Gets all models.
+     *
+     * @return Illuminate\Database\Eloquent\Collection|array;
+     */
+    public function all(): Collection|array;
 
-  /**
-   * Gets all models with query.
-   * 
-   * @param string $search
-   * @return Illuminate\Database\Eloquent\Collection|array;
-   */
-  public function search(string $search): Collection|array;
+    /**
+     * Gets all models with query.
+     *
+     * @param string $search
+     * @return Illuminate\Database\Eloquent\Collection|array;
+     */
+    public function search(string $search): Collection|array;
 
-  /**
-   * Find model by id.
-   * 
-   * @param int $id
-   * @return Illuminate\Database\Eloquent\Model|null;
-   */
-  public function findById(int $id): ?Model;
+    /**
+     * Find model by id.
+     *
+     * @param int $id
+     * @return Illuminate\Database\Eloquent\Model|null;
+     */
+    public function findById(int $id): ?Model;
 
-  /**
-   * Create a model.
-   * 
-   * @param array $payload
-   * @return Illuminate\Database\Eloquent\Model;
-   */
-  public function create(array $payload): Model;
+    /**
+     * Create a model.
+     *
+     * @param array $payload
+     * @return Illuminate\Database\Eloquent\Model;
+     */
+    public function create(array $payload): Model;
 
-  /**
-   * Update an existing model.
-   * 
-   * @param array $payload
-   * @param int $id
-   * @return Illuminate\Database\Eloquent\Model;
-   */
-  public function update(array $payload, int $id): Model;
+    /**
+     * Update an existing model.
+     *
+     * @param array $payload
+     * @param int $id
+     * @return Illuminate\Database\Eloquent\Model;
+     */
+    public function update(array $payload, int $id): Model;
 
-  /**
-   * Delete model by id.
-   * 
-   * @param int $id
-   * @return bool;
-   */
-  public function deleteById(int $id): bool;
+    /**
+     * Delete model by id.
+     *
+     * @param int $id
+     * @return bool;
+     */
+    public function deleteById(int $id): bool;
 }

846bcce20e02e652db201d385df3b9e342099f18
Luca Xue <lxue02@gmail.com>
2022-02-05 17:07:25 +0000

    Remove leading forward slash

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/books', BookController::class);
+Route::apiResource('books', BookController::class);
